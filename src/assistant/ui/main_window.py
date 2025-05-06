9b11369e755670ba3d0c7df86da2a444d685cf34
elise-love <elisadiskg5@gmail.com>
2025-05-06 10:02:25 +0800

    made it draggable

--- src/assistant/ui/main_window.py
+++ src/assistant/ui/main_window.py
@@ -1,5 +1,5 @@
 ﻿from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QSize,Qt
+from PyQt5.QtCore import QSize,Qt,QPoint
 import sys
 
 class MainWindow(QMainWindow):
@@ -19,10 +19,29 @@ class MainWindow(QMainWindow):
             border-radius: 20px;
         """)
 
+        self.is_dragging=False
+        self._drag_position = QPoint() #表示一個 2D 點，初始為 (0, 0)
             
+    def mousePressEvent(self,e):
+        if e.button()==Qt.LeftButton:
+            self.is_dragging = True
+            self._drag_position = e.globalPos() - self.frameGeometry().topLeft()
+            #e.globalPos()：回傳滑鼠在 螢幕上 的座標位置
 
+            e.accept()
 
+    def mouseMoveEvent(self, e):
+        if self.is_dragging and e.buttons() & Qt.LeftButton:
+            self.move(e.globalPos() - self._drag_position)
+            e.accept()
+        """
+        event.buttons() & Qt.LeftButton:
+            使用位元運算 & 來檢查 左鍵是否包含在目前被按的按鍵中。
+            如果有按下左鍵，就會是 True，否則是 False。
+        """
 
+    def mouseReleaseEvent(self,e):
+        self.is_dragging = False
 
 app = QApplication(sys.argv)
 
